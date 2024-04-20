fc489815cae9e4981f223155f7d83d1cb2614cb1
kosy-wer <protectorunmatched@gmail.com>
2024-04-20 08:03:11 +0700

    add user migrations and make fake data

--- cmd/register/main.go
+++ cmd/register/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "context"
     "log"
     "rest_api/internal/app/database"
 )
@@ -14,10 +15,22 @@ func main() {
     }
     defer db.Close()
 
+    ctx := context.Background()
+
+	script := "INSERT INTO users(username,password,email) VALUES('joko', 'password678','joko@gmail.com')"
+	_, err = db.ExecContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success insert new customer")
+
+
     // Test koneksi ke database
     if err := db.Ping(); err != nil {
         log.Fatalf("Error pinging database: %v", err)
     }
+
 
     fmt.Println("Successfully connected to the database!")
 }
